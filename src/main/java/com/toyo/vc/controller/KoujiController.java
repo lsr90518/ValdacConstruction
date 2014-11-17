1f89420c40d70eed3f4b1f304a40e428c1b3a8b0
lsr90518 <lb90518@gmail.com>
2014-11-17 15:58:33 +0900

    kouji/index

--- src/main/java/com/toyo/vc/controller/KoujiController.java
+++ src/main/java/com/toyo/vc/controller/KoujiController.java
@@ -1,6 +1,10 @@
 package com.toyo.vc.controller;
 
+import com.toyo.vc.service.KoujiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,9 +18,17 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/kouji")
 public class KoujiController {
 
+    @Autowired
+    KoujiService koujiService;
+
     @RequestMapping(method = RequestMethod.GET)
     public String index(HttpSession session){
 
+        return "index";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String getKoujiById(@PathVariable("id")String id, ModelMap modelMap, HttpSession session){
         return "index";
     }
 }
