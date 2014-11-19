3961fa5a56a8e482bbd2b4c5f344a8bdd7cda4ab
lsr90518 <lb90518@gmail.com>
2014-11-19 13:26:50 +0900

    responsibility -> person

---
+++ src/main/java/com/toyo/vc/controller/MasterController.java
@@ -0,0 +1,7 @@
+package com.toyo.vc.controller;
+
+/**
+ * Created by Lsr on 11/19/14.
+ */
+public class MasterController {
+}
---
+++ src/main/java/com/toyo/vc/dto/KoujiForm.java
@@ -0,0 +1,7 @@
+package com.toyo.vc.dto;
+
+/**
+ * Created by Lsr on 11/19/14.
+ */
+public class KoujiForm {
+}
